d0ddd6d7d78de2fafe68705c137862153bba9bb0
Guilherme Arendt <guiarendt006@gmail.com>
2023-05-04 21:51:36 -0300

    Add project files.

---
+++ Taxa/Entities/TaxPayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxa.Entities
+{
+    abstract class TaxPayer
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        public TaxPayer()
+        {
+        }
+        protected TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+        public abstract double Tax();
+    }
+}
